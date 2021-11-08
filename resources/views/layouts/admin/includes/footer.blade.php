e61e05aa9195ea715d0cb325a6e009d84c142b00
Bruno Santana <brunosantanafernandes@gmail.com>
2021-11-08 09:17:36 -0300

    FIX Ajustes css

--- resources/views/layouts/admin/includes/footer.blade.php
+++ resources/views/layouts/admin/includes/footer.blade.php
@@ -1,30 +1,30 @@
 <!-- jQuery -->
-<script src="{{ asset('admin/assets/js/jquery-3.5.0.min.js') }}"></script>
+<script src="{{ asset('assets/js/jquery-3.5.0.min.js') }}"></script>
 
 <!-- Plugins -->
-<script src="{{ asset('admin/assets/js/jquery.maskedinput.min.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.maskedinput.min.js') }}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
-<script src="{{ asset('admin/assets/js/mask.js') }}"></script>
-<script src="{{ asset('admin/assets/plugins/datatables/jquery.dataTables.min.js') }}"></script>
-<script src="{{ asset('admin/assets/plugins/datatables/datatables.min.js') }}"></script>
-<script src="{{ asset('admin/assets/js/crud-financial.js') }}"></script>
+<script src="{{ asset('assets/js/mask.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatables/datatables.min.js') }}"></script>
+<script src="{{ asset('assets/js/crud-financial.js') }}"></script>
 
 <!-- Bootstrap Core  -->
-<script src="{{ asset('admin/assets/js/popper.min.js') }}"></script>
-<script src="{{ asset('admin/assets/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/js/popper.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
 
 <!-- Slimscroll -->
-<script src="{{ asset('admin/assets/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
 
 <!-- Custom -->
-<script src="{{ asset('admin/assets/js/admin.js') }}"></script>
-<script src="{{ asset('admin/assets/js/functions.js') }}"></script>
-<script src="{{ asset('admin/assets/js/bootstrap-tagsinput.min.js') }}"></script>
+<script src="{{ asset('assets/js/admin.js') }}"></script>
+<script src="{{ asset('assets/js/functions.js') }}"></script>
+<script src="{{ asset('assets/js/bootstrap-tagsinput.min.js') }}"></script>
 <script src="//cdn.ckeditor.com/4.16.1/full/ckeditor.js"></script>
 
 
 <!-- Select2 -->
-<script src="{{ asset('admin/assets/js/select2.min.js') }}"></script>
+<script src="{{ asset('assets/js/select2.min.js') }}"></script>
 
 @livewireScripts
 
--- resources/views/layouts/admin/includes/head.blade.php
+++ resources/views/layouts/admin/includes/head.blade.php
@@ -4,12 +4,12 @@
 <meta content="Proconsig | Software de Gestão para Consignados" name="description" />
 <meta content="RV6 Tecnologia" name="author" />
 <!-- App favicon -->
-<link rel="shortcut icon" href="{{ asset('admin/assets/images/favicon.ico') }}">
+<link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
 
 <!-- Bootstrap Css -->
-<link href="{{ asset('admin/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet"
+<link href="{{ asset('assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet"
     type="text/css" />
 <!-- Icons Css -->
-<link href="{{ asset('admin/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
 <!-- App Css-->
-<link href="{{ asset('admin/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
