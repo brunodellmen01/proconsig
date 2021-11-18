4ce868711cd44cd40d4a43b880ac49a722a5f2c4
unknown <ge.cataneo@gmail.com>
2021-11-18 07:46:47 -0300

    push

--- resources/views/admin/system/includes/nav-bar.blade.php
+++ resources/views/admin/system/includes/nav-bar.blade.php
@@ -4,17 +4,10 @@
         <ul class="navbar-nav">
 
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle arrow-none" href="#" id="topnav-dashboard" role="button">
+                <a class="nav-link dropdown-toggle arrow-none" href="{{ route('admin.dash') }}" id="topnav-dashboard"
+                    role="button">
                     <i class="bx bx-home-circle me-2"></i><span key="t-dashboards">Dashboards</span>
-                    <div class="arrow-down"></div>
                 </a>
-                <div class="dropdown-menu" aria-labelledby="topnav-dashboard">
-
-                    <a href="index.html" class="dropdown-item" key="t-default">Default</a>
-                    <a href="dashboard-saas.html" class="dropdown-item" key="t-saas">Saas</a>
-                    <a href="dashboard-crypto.html" class="dropdown-item" key="t-crypto">Crypto</a>
-                    <a href="dashboard-blog.html" class="dropdown-item" key="t-blog">Blog</a>
-                </div>
             </li>
 
             <li class="nav-item dropdown">
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8" />
+    <title>404 - Página não Encontrada | Proconsig</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('backend/images/favicon.png') }}') }}">
+
+    <!-- Bootstrap Css -->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+    <div class="account-pages my-5 pt-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-center mb-5">
+                        <h1 class="display-2 fw-medium">4<i class="bx bx-buoy bx-spin text-primary display-3"></i>4</h1>
+                        <h4 class="text-uppercase">Desculpe página não encontrada</h4>
+                        <div class="mt-5 text-center">
+                            <a class="btn btn-primary waves-effect waves-light" href="{{ route('admin.dash') }}">Voltar
+                                ao Dashboard</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-8 col-xl-6">
+                    <div>
+                        <img src="{{ asset('assets/images/error-img.png') }}" alt="" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @include('admin.system.includes.script-js')
+
+</body>
+
+</html>
