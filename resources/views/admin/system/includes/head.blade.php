c3b342862e68467c701c415df52001a50a94aaea
Bruno Santana <brunosantanafernandes@gmail.com>
2021-11-18 13:35:24 -0300

    FIX Ajustes de subindo datatables

--- resources/views/admin/system/includes/head.blade.php
+++ resources/views/admin/system/includes/head.blade.php
@@ -16,5 +16,11 @@
     <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('assets/css/app.css') }}" rel="stylesheet" type="text/css">
     <link href="{{ asset('backend/css/messages.css') }}" rel="stylesheet" type="text/css">
+    <!-- DataTables -->
+    <link href="{{ asset('assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet"
+        type="text/css" />
+    <link href="{{ asset('assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css') }}" rel="stylesheet"
+        type="text/css" />
+
 
 </head>
